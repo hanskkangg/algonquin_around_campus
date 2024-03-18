096747bd7acead8c53348c35fcb6f73375096ce7
hanskkangg <hans.kkang@gmail.com>
2024-03-18 16:18:25 -0400

    Second Commit - Implemented RecyclerView and adapter class. The app is now running with a hardcoded dataset of 10 images and store information.

--- app/src/main/java/algonquin/cst2335/algonquin_around_campus/Adapter.java
+++ app/src/main/java/algonquin/cst2335/algonquin_around_campus/Adapter.java
@@ -1,4 +1,44 @@
 package algonquin.cst2335.algonquin_around_campus;
 
-public class Adapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    LayoutInflater inflater;
+
+    public Adapter(Context context){
+        this.inflater = LayoutInflater.from(context);
+
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.custom_layout,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        //Showing 10 items
+        return 10;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+    }
 }
--- app/src/main/java/algonquin/cst2335/algonquin_around_campus/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/algonquin_around_campus/MainActivity.java
@@ -1,14 +1,26 @@
 package algonquin.cst2335.algonquin_around_campus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    Adapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.ListOfData);
+        adapter = new Adapter(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+
     }
 }
