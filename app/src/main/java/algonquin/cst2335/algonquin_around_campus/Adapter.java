6d948fa9350007bd06e7c3e414e8203748c38483
hanskkangg <hans.kkang@gmail.com>
2024-03-18 15:10:27 -0400

    Initial Commit - Start of the Algonquin_around_campus app dev

---
+++ app/src/main/java/algonquin/cst2335/algonquin_around_campus/Adapter.java
@@ -0,0 +1,4 @@
+package algonquin.cst2335.algonquin_around_campus;
+
+public class Adapter {
+}
---
+++ app/src/main/java/algonquin/cst2335/algonquin_around_campus/MainActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.algonquin_around_campus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
